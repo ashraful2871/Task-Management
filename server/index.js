a7d4a6d9ac30eb27edd46c87233b73e7a2f411d5
ashraful2871 <ashrafulislam2758@gmail.com>
2025-02-20 19:00:09 +0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send("task management is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
