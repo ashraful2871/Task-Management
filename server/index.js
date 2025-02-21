f966ce01861a7a49ee89ac7a2743b91cbbb98efd
ashraful2871 <ashrafulislam2758@gmail.com>
2025-02-21 09:46:42 +0600

    addd task in save in datbase

--- server/index.js
+++ server/index.js
@@ -7,6 +7,45 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+//task-management
+//lsjZe94BwTvDsAik
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.jq7qb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const db = client.db("TaskDB");
+    const taskCollection = db.collection("tasks");
+
+    app.post("/add-task", async (req, res) => {
+      const { taskInfo } = req.body;
+      const result = await taskCollection.insertOne(taskInfo);
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", async (req, res) => {
   res.send("task management is running");
